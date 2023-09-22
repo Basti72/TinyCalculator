92a70834035e6d4abe47097dbd55e2e3a6d39076
BASTI-LAPTOP\obern <oberndorfer.sebastian@gmail.com>
2023-09-22 09:02:18 +0200

    Create simple division calculator

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=== Tiny Calculator ===");
+Console.Write("Dividend: ");
+int dividend = int.Parse(Console.ReadLine());
+Console.Write("Divisor: ");
+int divisor = int.Parse(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Result: {(float)dividend/(float)divisor}");
+Console.ResetColor();
