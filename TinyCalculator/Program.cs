5073057a582e9ec3afe37776493f48994c253a02
BASTI-LAPTOP\obern <oberndorfer.sebastian@gmail.com>
2023-09-29 09:14:47 +0200

    improved error-handling

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,23 +1,26 @@
 ﻿Console.WriteLine("=== Tiny Calculator ===");
-bool x;
-int dividend, divisor;
-do
-{
-    Console.Write("Dividend: ");
-    x = int.TryParse(Console.ReadLine(), out dividend);
-}
-while (!x);
 
-do
-{
-    Console.Write("Divisor: ");
-    x = int.TryParse(Console.ReadLine(), out divisor);
-}
-while (!x);
-
-if(divisor == 0)
-    throw new DivideByZeroException("Cannot divide by 0!");
+int dividend = GetNumber("Enter dividend: ");
+int divisor = GetNumber("Enter divisor: ");
 
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine($"\nResult: {(float)dividend/divisor}");
 Console.ResetColor();
+
+static int GetNumber(string text)
+{
+    uint attempts = 0;
+    while(true)
+    {
+        Console.Write(text);
+        if (int.TryParse(Console.ReadLine(), out var number))
+            return number;
+        Console.WriteLine("Please enter a valid number!\n");
+        attempts++;
+        if(attempts == 10)
+        {
+            Console.WriteLine("You are too fucking dumb! Bye");
+            Environment.Exit(0);
+        }
+    }
+}
