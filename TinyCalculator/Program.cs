df3fbecfa82f9f0460d8a4082d83c9ba7393850a
BASTI-LAPTOP\obern <oberndorfer.sebastian@gmail.com>
2023-10-13 09:22:37 +0200

    added operators +, -, *

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,17 +1,43 @@
 ﻿Console.WriteLine("=== Tiny Calculator ===");
 
-int dividend = GetNumber("Enter dividend: ");
-int divisor = GetNumber("Enter divisor: ");
+int number1 = GetNumber("Enter first number: ");
+char sign = GetOperator("Enter operator: ");
+int number2 = GetNumber("Enter second number: ");
 
-if(divisor == 0)
+if(sign == '/' && number2 == 0)
 {
     Console.WriteLine("Divisor cannot be 0!");
     return;
 }
 
 Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine($"\nResult: {(float)dividend/divisor}");
+switch(sign)
+{
+    case '+': Console.WriteLine($"\nResult: {number1 + number2}"); break;
+    case '-': Console.WriteLine($"\nResult: {number1 - number2}"); break;
+    case '*': Console.WriteLine($"\nResult: {number1 * number2}"); break;
+    case '/': Console.WriteLine($"\nResult: {number1 / number2}"); break;
+}
 Console.ResetColor();
+
+static char GetOperator(string text)
+{
+    uint attempts = 0;
+    while (true)
+    {
+        Console.Write(text);
+        if (char.TryParse(Console.ReadLine(), out var sign) && (sign == '+' || sign == '-' || sign == '*' || sign == '/'))
+            return sign;
+
+        Console.WriteLine("Please enter a valid operator [+, -, /, *]!\n");
+        attempts++;
+        if (attempts == 10)
+        {
+            Console.WriteLine("You are too fucking dumb! Bye");
+            Environment.Exit(0);
+        }
+    }
+}
 
 static int GetNumber(string text)
 {
